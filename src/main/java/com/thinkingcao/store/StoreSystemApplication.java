e6276463050a6c1f8a398c21ad20593c3d5a7636
Thinkingcao <617271837@qq.com>
2020-09-22 14:15:39 +0800

    修改启动类

--- src/main/java/com/thinkingcao/store/StoreSystemApplication.java
+++ src/main/java/com/thinkingcao/store/StoreSystemApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @MapperScan: 扫描Mapper文件夹
+ */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.thinkingcao.store.system.mapper"})
 public class StoreSystemApplication {
